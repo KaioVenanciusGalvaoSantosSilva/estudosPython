944f110305c38284247fc180f7bff286d7df7177
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-03-18 22:28:32 -0300

    Testes e Depuração

---
+++ Testes/depuracao.py
@@ -0,0 +1,17 @@
+import calculadora as calc
+
+teste = 'sou uma string'
+primeiravariavel = 5
+segundavariavel = 6
+soma= 0
+
+soma = primeiravariavel + segundavariavel
+old = teste
+teste = 'vazio'
+
+print('Fim')
+resultado =''
+resultado = calc.adicionar(primeiravariavel, segundavariavel)
+
+print(resultado)
+
