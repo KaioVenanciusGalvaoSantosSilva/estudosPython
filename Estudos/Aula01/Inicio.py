8f01bf173d9264711b5591bd16bd9c6dbbe8cb97
Kaio Galvao <kaioifsp@hotmail.com>
2024-05-10 20:26:11 -0300

    Aula 01 Introdução

---
+++ Estudos/Aula01/Inicio.py
@@ -0,0 +1,35 @@
+# Principais motivos para se estudar programação
+
+# Te torna uma pessoa mais inteligênte.
+# Melhora seu raciocínio.
+
+# De acordo com a OCDE o crescimento tecnológico é uma realidade e que até 2027 tem estimativa de 1.4 milhão de vagas de desenvolvedor.
+# Hoje muitas das coisas que são utilizadas no mundo moderno envolve programação aplicativo, smartwatch, geladeira, dispositivos inteligentes entre outras coisas.
+
+# https://www.youtube.com/watch?v=CLf7fxqltgg
+
+# Buscamos resolver problemas.
+
+# Requisitos:
+    # *Disciplina
+    # *Força de vontade
+    # *Um dispositivo
+
+# Obrigatório:
+    # *Compromisso
+    # *Paciência
+    # *Prática
+    # *Base sólida (lógica de programação)
+
+# Principais profissionais de Desenvolvimento
+    # Desktop (programas, app, jogos)
+    # Web (sites)
+    # Mobile (jogos e apps)
+    # Iot (sistemas embarcados)
+
+#REF https://www.youtube.com/watch?v=S9uPNppGsGo&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6
+
+#Observações:
+    # O nerd de hoje é o bilionário de amanhã
+    # Persista que valerá a pena
+    # Lembrete para daqui 5 anos -- 'Valeu a pena cada momento de estudo'.
---
+++ Testes/teste3.py
@@ -0,0 +1,9 @@
+
+
+n1 = float (input("Digite um número: "))
+n2 = int (input("digite um número: "))
+s = n1+n2
+nome = 'funciona'
+
+print("O resultado é ", s,  nome)
+print("A soma {}".format(nome) )
