71dd94583eaab377dcab87c1a933c6766b6baa3b
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-05-10 23:18:30 -0300

    Exercicio desafio002

---
+++ Estudos/Exericios/Exercicios Corrigidos/desafio002.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+
+nome = input("Digite seu nome: ")
+
+#print formatado
+print("Seja bem-vindo, {}!".format(nome))
+#print("Seja bem-vindo,", nome)
+#print("Seja bem-vindo, " + nome)
---
+++ Estudos/Exericios/desafio002.py
@@ -0,0 +1,1 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
