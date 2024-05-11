ffe4e09a505a50a795d6f8d995efde90a5c167f5
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-05-10 22:55:25 -0300

    Instalando PyCharm QPython3 Google Colab

---
+++ Estudos/Aula05/Aula teoria.py
@@ -0,0 +1,4 @@
+#Instalação do PyCharm
+#Interpretador para celular QPython3
+#Pode usar o Colab do Google ou Jupyter Notebook para codar e interpretar online 
+# https://colab.research.google.com/ ou https://jupyter.org/try-jupyter/notebooks/?path=notebooks/Intro.ipynb
