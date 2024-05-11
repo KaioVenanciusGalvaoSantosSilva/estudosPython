64c0804a9723856e04bf9818f6de4e21650fbbb1
Kaio Galvao <kaioifsp@hotmail.com>
2024-05-10 21:30:05 -0300

    Aula 03 Instalar Python

---
+++ Estudos/Aula03/instalarPython.py
@@ -0,0 +1,17 @@
+#Instalação Python no Windows
+
+#Para usuarios do Sistema Operacional Windows precisará instalar o interpretador (programa que irá compreender a linguagem python)
+
+# Para locais que não tem acesso ao site https://www.python.org/downloads/windows/ baixei o python e subi no repositório 'python-3.12.3-64bits' ou 'python-3.12.3-32bits'
+#Basta baixar e instalar
+#Durante a instalação marque a caixa 'add Python 3 to PATH' para funcionar no terminal
+#Ao finalizar a instalação lembre-se de clicar em 'Disable path length limit'
+
+#Abra o cmd (prompt de comando)
+#Digite python
+#Agora já estará usando o interpretador.
+
+#Para facilitar o processo de aprendizagem e desenvolvimento recomendo que use IDEs como VSCode, Pycharm entre outros
+#Deixarei o VSCode disponível no repositório 
+#Site oficial VSCODE https://code.visualstudio.com/
+#No VSCODE basta procurar por extensoes no menu lateral e baixar a extensão python
