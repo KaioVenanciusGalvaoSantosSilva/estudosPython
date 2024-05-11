a4070bbfc9221bd27355f51cc076317b789eb32a
Kaio Galvao <kaioifsp@hotmail.com>
2024-05-10 21:09:36 -0300

    Sobre o Python

---
+++ Estudos/Aula02/whatisPython.py
@@ -0,0 +1,72 @@
+#https://www.python.org/doc/essays/blurb/
+#https://www.python.org/about/gettingstarted/
+
+#Site Oficial https://www.python.org/
+#Criador
+#Guido Van Hossum
+#Linguagem de programação interpretada e de simples compreensão
+#Nome dado por causa do programa de TV 'Monty Python's Flying Circus'
+#Posteriormente foi associado a serpente piton (python).
+
+#CP4E - Programação de computadores para todos
+#passou a ser PSF - Python Software Foundation
+#Python é uma linguagem de programação de alto nível (mais próximo da linguagem humana)
+
+#Empresas que utilizam python
+
+    # Google
+    # Microsoft
+    # Nasa
+    # Facebook
+    # Instagram
+    # Dropbox
+    # Spotify
+    # Reddit
+    # ++
+
+# Python é:
+    #Python é nativo em diversos sistemas operacionais exceto no Windows
+    #Linguagem de propósito Geral
+    #intuitivo
+    #multiplataforma
+    #batteries included (com bibliotecas nativas)
+    #livre
+    #exige organização no código
+    #OO Orientação a objetos
+
+   
+
+# The Zen of Python, by Tim Peters
+    # #https://peps.python.org/pep-0020/
+    # import this
+
+        # É uma linguagem de programação que é:
+
+        # Bonita é melhor que feia.
+        # Explícito é melhor que implícito.
+        # Simples é melhor que complexo.
+        # Complexo é melhor que complicado.
+        # Plano é melhor que aninhado.
+        # Esparso é melhor que denso.
+        # Legibilidade conta.
+        # Casos especiais não são especiais o bastante para quebrar as regras.
+        # Embora a praticidade vença a pureza.
+        # Erros nunca devem passar silenciosamente.
+        # A menos que explicitamente silenciados.
+        # Diante da ambigüidade, recuse a tentação de adivinhar.
+        # Deve haver um, e preferencialmente apenas um, modo óbvio para fazê-lo.
+        # Embora esse modo possa não ser óbvio a princípio a menos que você seja holandês.
+        # Agora é melhor que nunca.
+        # Embora nunca seja muitas vezes melhor que agora mesmo.
+        # Se a implementação é difícil de explicar, é uma má idéia.
+        # Se a implementação é fácil de explicar, pode ser uma boa idéia.
+        # Namespaces são uma grande idéia, vamos fazer mais dessas!
+
+#Jogos que usam Python
+    #Civilization 4
+    # BattleField 2
+    # EVEOnline
+    # ++
+
+
+
