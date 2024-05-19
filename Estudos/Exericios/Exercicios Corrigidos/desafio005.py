27302e906896eee168d00650de504b4e83a895a2
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-05-19 17:10:05 -0300

    Exercicio desafio 005

---
+++ Estudos/Exericios/Exercicios Corrigidos/desafio005.py
@@ -0,0 +1,7 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+
+numero = int(input("Digite um número inteiro para descobrir seu antecessor e seu sucessor: "))
+
+print("Numero: {}".format(numero))
+print("O seu antecessor é: {}".format(numero-1))
+print("O seu sucessor é: {}".format(numero+1))
---
+++ Estudos/Exericios/desafio005.py
@@ -0,0 +1,1 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
