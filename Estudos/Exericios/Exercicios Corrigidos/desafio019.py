86d748b8b71e75bc55768518ade13e141da34e84
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-05-25 17:40:38 -0300

    Exercicio desafio 019

---
+++ Estudos/Exericios/Exercicios Corrigidos/desafio019.py
@@ -0,0 +1,14 @@
+#Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+#Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+from random import choice
+
+aluno1 = input("Digite o nome do primeiro aluno: ")
+aluno2 = input("Digite o nome do segundo aluno: ")
+aluno3 = input("Digite o nome do terceiro aluno: ")
+aluno4 = input("Digite o nome do quarto aluno: ")
+
+alunos = [aluno1,aluno2,aluno3,aluno4]
+
+sorteado = choice(alunos)
+print("O aluno sorteado foi: {}".format(sorteado))
---
+++ Estudos/Exericios/desafio019.py
@@ -0,0 +1,2 @@
+#Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+#Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
