b33d6157f9e1938e5f4094d080cb80afe1666466
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-05-28 16:29:53 -0300

    Exercicio desafio 032

--- Estudos/Exericios/Exercicios Corrigidos/desafio032.py
+++ Estudos/Exericios/Exercicios Corrigidos/desafio032.py
@@ -11,3 +11,4 @@ if ((ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0):
     print(f"O ano {ano} é bissexto.")
 else:
     print(f"O ano {ano} não é bissexto.")
+
--- Estudos/Exericios/desafio032.py
+++ Estudos/Exericios/desafio032.py
@@ -1,1 +1,2 @@
 #Faça um programa que leia um ano qualquer e mostra se ele é bissexto ou não.
+
