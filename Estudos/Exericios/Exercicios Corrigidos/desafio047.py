9b307ec1ae342b50bedad9d100d98846a2eda971
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-05-30 18:18:02 -0300

    Exercicio desafio 047

---
+++ Estudos/Exericios/Exercicios Corrigidos/desafio047.py
@@ -0,0 +1,8 @@
+#Crie um programa que mostre na tela todos os números pares que estão entre 1 e 50
+
+#for c in range(2,50,2):
+#     print(c)
+
+for c in range(1,50):
+    if c % 2 == 0:
+        print(c)
---
+++ Estudos/Exericios/desafio047.py
@@ -0,0 +1,1 @@
+#Crie um programa que mostre na tela todos os números pares que estão entre 1 e
