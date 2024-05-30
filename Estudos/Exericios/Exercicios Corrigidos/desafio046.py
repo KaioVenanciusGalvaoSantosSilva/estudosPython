f1b915099311523fdc0e8827180f83300e61b7ef
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-05-30 18:10:15 -0300

    Exercicio desafio 046

---
+++ Estudos/Exericios/Exercicios Corrigidos/desafio046.py
@@ -0,0 +1,7 @@
+#Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+import time
+for c in range(10,-1,-1):
+    print(c)
+    time.sleep(1)
+
+print("Boooom")
---
+++ Estudos/Exericios/desafio046.py
@@ -0,0 +1,1 @@
+#Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
