613e9c89f9a9e7d34fcbe645f7f4eb8f204b3ea2
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-05-30 18:29:20 -0300

    Exercicio desafio 049

---
+++ Estudos/Exericios/Exercicios Corrigidos/desafio049.py
@@ -0,0 +1,25 @@
+#Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+# Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada
+
+# numero = int(input("Digite qualquer número inteiro: "))
+
+# print( "Tabuada do número {}".format(numero))
+# print( "{} x 0 = {}".format(numero, numero*0))
+# print( "{} x 1 = {}".format(numero, numero*1))
+# print( "{} x 2 = {}".format(numero, numero*2))
+# print( "{} x 3 = {}".format(numero, numero*3))
+# print( "{} x 4 = {}".format(numero, numero*4))
+# print( "{} x 5 = {}".format(numero, numero*5))
+# print( "{} x 6 = {}".format(numero, numero*6))
+# print( "{} x 7 = {}".format(numero, numero*7))
+# print( "{} x 8 = {}".format(numero, numero*8))
+# print( "{} x 9 = {}".format(numero, numero*9))
+# print( "{} x 10 = {}".format(numero, numero*10))
+#-----------------------------------------------------------
+
+numero = int(input("Digite qualquer número inteiro que deseja saber a tabuada: "))
+
+for c in range(0,11):
+    print( "{} x {} = {}".format(numero, c, numero*c))
+    
---
+++ Estudos/Exericios/desafio049.py
@@ -0,0 +1,1 @@
+#Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
