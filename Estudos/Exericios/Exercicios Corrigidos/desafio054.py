ea4c215f542ad81f155724c2150b62a946ba2a0a
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-06 21:43:13 -0300

    Exercicio desafio 054

---
+++ Estudos/Exericios/Exercicios Corrigidos/desafio054.py
@@ -0,0 +1,20 @@
+#Crie um programa que leia o ano de nascimento de 7 pessoas.
+#No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+import datetime
+pessoas = []
+for i in range(7):
+    ano_nascimento = int(input("Digite o ano de nascimento da pessoa {}: ".format(i + 1)))
+    pessoas.append(ano_nascimento)
+
+ano_atual = datetime.datetime.now().year
+
+maiores = 0
+menores = 0
+
+for j in range(7):
+    if ano_atual - pessoas[j] >= 18:
+        maiores += 1
+    else:
+        menores += 1
+
+print("{} pessoas ainda não atingiram a maioridade e {} já são maiores.".format(menores,maiores))
---
+++ Estudos/Exericios/desafio054.py
@@ -0,0 +1,2 @@
+#Crie um programa que leia o ano de nascimento de 7 pessoas.
+#No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
