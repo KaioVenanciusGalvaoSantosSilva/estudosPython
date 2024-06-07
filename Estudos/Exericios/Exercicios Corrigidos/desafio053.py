668c83ec0fd1ce1130ef2f657eb80603e0ee6512
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-06 21:30:04 -0300

    Exercicio desafio 053

---
+++ Estudos/Exericios/Exercicios Corrigidos/desafio053.py
@@ -0,0 +1,9 @@
+#Crie um programa que leie uma frase qualquer e diga se ela é um palindromo desconsiderando os espaços.
+
+frase = str(input("Digite uma frase: "))
+palindromo = frase.split()
+reverse = palindromo[::-1]
+if palindromo == reverse:
+    print("Sim, a frase '{}' é um palindromo!".format(frase))
+else:
+    print("Não, a frase '{}' não é um palindromo!".format(frase))
---
+++ Estudos/Exericios/desafio053.py
@@ -0,0 +1,1 @@
+#Crie um programa que leie uma frase qualquer e diga se ela é um palindromo desconsiderando os espaços.
